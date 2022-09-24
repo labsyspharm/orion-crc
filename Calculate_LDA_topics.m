8d10f62507f6aac5988be6d1d21332178b5e73fb
Jerry Lin <jiaren.lin@gmail.com>
2022-09-24 00:08:40 -0400

    Update Calculate_LDA_topics.m

--- Calculate_LDA_topics.m
+++ Calculate_LDA_topics.m
@@ -1,25 +1,49 @@
-%% Calculate topic neighborhood (All slides);
+%% Counts tables for all slides (only markers)
 
 tic;
-Alltopic2topic = zeros(12,12,length(slideName));
+maxL = 200;
+allcounts = [];
 
-for i = 1:length(slideName)
-    disp(strcat('Processing:',slideName{i}));
-    data1 = eval(strcat('data',slideName{i}));
-    data2 = varfun(@mean,data1,'GroupingVariable','frame');
-    temp1 = data2.Properties.VariableNames;
-    temp1 = regexprep(temp1,'mean_','');
-    data2.Properties.VariableNames = temp1;
+for s=1:length(slideName)
+    disp(strcat('Processing:',slideName{s}));
+    data1 = eval(strcat('data',slideName{s}));
 
-    list1 = knnsearch(data2{:,{'Xt','Yt'}},data2{:,{'Xt','Yt'}},'k',10);
-    list2 = data2.topics(list1);
+    data1.col = round(data1.Xt ./ maxL)+1;
+    data1.row = round(data1.Yt ./ maxL)+1;
+    data1.frame = round(data1.col+ max(data1.col)*(data1.row-1));
 
-    topic2topic = zeros(12,12);
-
-    for k = 2:10
-        temp1 = hist3(list2(:,[1,k]),{1:12 1:12});
-        topic2topic = topic2topic+temp1;
+    count1 = varfun(@sum,data1,'GroupingVariables','frame','Inputvariables',labelp);
+    if isempty(allcounts)
+        allcounts = count1{:,3:end};
+    else
+        allcounts = vertcat(allcounts,count1{:,3:end});
     end
-    Alltopic2topic(:,:,i) = topic2topic;
+    eval(strcat('data',slideName{s},'=data1;'));
     toc;
+end   
+
+%% --Calculate LDA & plots (markers only, All slides)-----
+
+maxT = 30;
+tic;
+lda1 = fitlda(allcounts,maxT);
+toc;
+figure,imagesc(lda1.TopicWordProbabilities);colormap(jet);
+title ('Topic Word probabilities');
+set(gca,'ytick',1:length(labelp2));
+set(gca,'yticklabels',labelp2);
+xlabel('Topics');
+colorbar;
+caxis([0 0.25]);
+
+figure
+for topicIdx = 1:maxT
+    subplot(5,6,topicIdx)
+    temp1 = table;
+    temp1.Word = labelp2;
+    
+    temp1.Count = lda1.TopicWordProbabilities(:,topicIdx);
+    wordcloud(temp1,'Word','Count');
+    title("Topic: " + topicIdx)
 end
+toc;
